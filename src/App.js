72af07e5bc9949357cabfa517019496b8261b723
venkatesh <venkateshrs555@gmail.com>
2021-02-22 21:44:22 +0530

    created board and squre

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Board from "./components/Board";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
+const App = () => {
+  return (
+  <div>
+    <h1>TIC TAC TOE</h1>
     <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+    <Board />
+  </div>
+  );
+ } 
+ 
+ export default App;
+
